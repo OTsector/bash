6d9996e9d5d862babde1f5d779d0c2e6e78f5df9
OT <imot@protonmail.com>
2018-06-19 16:58:39 +0400

    Update lightdm-autologin.sh

--- lightdm-autologin.sh
+++ lightdm-autologin.sh
@@ -2,7 +2,7 @@
 {
 	sed 's/#autologin-user=/autologin-user=root/g' -i /etc/lightdm/lightdm.conf
 	sed 's/#autologin-user-timeout=0/autologin-user-timeout=0/g' -i /etc/lightdm/lightdm.conf
-	sed 's/auth required pam_succeed_if.so user \!\= root quiet_success/#auth required pam_succeed_if.so user \!\= root quiet_success/g' -i /etc/pam.d/lightdm-autologin
+	sed 's/auth      required pam_succeed_if.so user != root quiet_success/#auth      required pam_succeed_if.so user != root quiet_success/g' -i /etc/pam.d/lightdm-autologin
 } &> /dev/null
 echo "Well Done!"
 exit 0
