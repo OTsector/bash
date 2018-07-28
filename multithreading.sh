00b0fb6ff2b8efc6d4f2b02198345396ab7b15af
OT <imot@protonmail.com>
2018-07-28 20:23:42 +0000

    Add files via upload

--- multithreading.sh
+++ multithreading.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
 if [[ $# < 2 ]]; then
 	echo -e "usage: ./script.sh [website] [threads]"; exit 1
 fi
+
 url=$1
 threads=$2
 counter=0
@@ -10,12 +12,15 @@ while [[ true ]]; do
 	function request {
 		echo done with status code: $(curl $url -s -w "%{http_code}" -o /dev/null)
 	}
+
 		echo "building threads..."
 		for (( i = 1; i <= $threads; i++ )); do
 			request &
 		done
 		wait
+
 		let counter=$counter+$threads
 		echo sent: $counter request
+
 done
 exit 0
