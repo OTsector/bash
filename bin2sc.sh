8ff39af01d27354e26fce37c53ade0b77f9cf188
OT <imot@protonmail.com>
2019-07-25 05:18:57 +0400

    Update bin2sc.sh

--- bin2sc.sh
+++ bin2sc.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 if [ -f /usr/bin/hexdump ]; then
-	hexdump -v -e '"\\""x" 1/1 "%02x" ""' $1
+	if [ $# -gt 0 ]]; then
+		hexdump -v -e '"\\""x" 1/1 "%02x" ""' $1
+	else
+		echo "use: "$0" [file.bin]"; exit 1
 else
 	echo "ERROR: hexdump is not installed"; exit 1
 	echo -e "install command: \"sudo apt install hexdump\""
