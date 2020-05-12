a95f9dc3863d74db85cc13f5116dce6f34bb8b17
OT <imot@protonmail.com>
2020-05-12 06:36:46 -0400

    Update ccache.sh

--- ccache.sh
+++ ccache.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-updatedb&&locate cache|grep 'cache/'|sed 's/cache\/.*/cache\//g'|sort|uniq|while read dir; do rm -rf "$dir"*; done&&echo done\!
+updatedb&&locate cache|grep 'cache/'|sed 's/cache\/.*/cache\//g;/\/usr\/share\//d'|sort|uniq|while read dir; do rm -rf "$dir"*; done&&echo done\!
