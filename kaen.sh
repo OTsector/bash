2332883a54cbf4e7424c28d42e6166de44143456
OT <imot@protonmail.com>
2020-07-01 16:06:38 +0400

    Add files via upload

--- kaen.sh
+++ kaen.sh
@@ -5,21 +5,21 @@ if [ $# -lt 2 ] || [ ! -f $1 ]; then
 	exit 1
 fi
 
-cat $1|sed 's/ა/a/g'|sed 's/ბ/b/g'|sed 's/გ/g/g'|sed 's/დ/d/g'|sed 's/ე/e/g' \
-	|sed 's/ვ/v/g'|sed 's/ზ/z/g'|sed 's/თ/t/g'|sed 's/ი/i/g'|sed 's/კ/k/g' \
-	|sed 's/ლ/l/g'|sed 's/მ/m/g'|sed 's/ნ/n/g'|sed 's/ო/o/g'|sed 's/პ/p/g' \
-	|sed 's/ჟ/zh/g'|sed 's/რ/r/g'|sed 's/ს/s/g'|sed 's/ტ/t/g'|sed 's/უ/u/g' \
-	|sed 's/ფ/f/g'|sed 's/ქ/q/g'|sed 's/ღ/gh/g'|sed 's/ყ/k/g'|sed 's/შ/sh/g' \
-	|sed 's/ჩ/ch/g'|sed 's/ც/c/g'|sed 's/ძ/dz/g'|sed 's/წ/ts/g'|sed 's/ჭ/tch/g' \
-	|sed 's/ხ/kh/g'|sed 's/ჯ/j/g'|sed 's/ჰ/h/g' > $2
+cat $1|sed 's/ა/a/g;s/ბ/b/g;s/გ/g/g;s/დ/d/g;s/ე/e/g' \
+	|sed 's/ვ/v/g;s/ზ/z/g;s/თ/t/g;s/ი/i/g;s/კ/k/g' \
+	|sed 's/ლ/l/g;s/მ/m/g;s/ნ/n/g;s/ო/o/g;s/პ/p/g' \
+	|sed 's/ჟ/zh/g;s/რ/r/g;s/ს/s/g;s/ტ/t/g;s/უ/u/g' \
+	|sed 's/ფ/f/g;s/ქ/q/g;s/ღ/gh/g;s/ყ/k/g;s/შ/sh/g' \
+	|sed 's/ჩ/ch/g;s/ც/c/g;s/ძ/dz/g;s/წ/ts/g;s/ჭ/tch/g' \
+	|sed 's/ხ/kh/g;s/ჯ/j/g;s/ჰ/h/g' > $2
 
-cat $1|sed 's/ა/a/g'|sed 's/ბ/b/g'|sed 's/გ/g/g'|sed 's/დ/d/g'|sed 's/ე/e/g' \
-	|sed 's/ვ/v/g'|sed 's/ზ/z/g'|sed 's/თ/t/g'|sed 's/ი/i/g'|sed 's/კ/k/g' \
-	|sed 's/ლ/l/g'|sed 's/მ/m/g'|sed 's/ნ/n/g'|sed 's/ო/o/g'|sed 's/პ/p/g' \
-	|sed 's/ჟ/j/g'|sed 's/რ/r/g'|sed 's/ს/s/g'|sed 's/ტ/t/g'|sed 's/უ/u/g' \
-	|sed 's/ფ/f/g'|sed 's/ქ/q/g'|sed 's/ღ/g/g'|sed 's/ყ/y/g'|sed 's/შ/sh/g' \
-	|sed 's/ჩ/ch/g'|sed 's/ც/c/g'|sed 's/ძ/dz/g'|sed 's/წ/w/g'|sed 's/ჭ/tch/g' \
-	|sed 's/ხ/x/g'|sed 's/ჯ/j/g'|sed 's/ჰ/h/g' >> $2
+cat $1|sed 's/ა/a/g;s/ბ/b/g;s/გ/g/g;s/დ/d/g;s/ე/e/g' \
+	|sed 's/ვ/v/g;s/ზ/z/g;s/თ/t/g;s/ი/i/g;s/კ/k/g' \
+	|sed 's/ლ/l/g;s/მ/m/g;s/ნ/n/g;s/ო/o/g;s/პ/p/g' \
+	|sed 's/ჟ/j/g;s/რ/r/g;s/ს/s/g;s/ტ/t/g;s/უ/u/g' \
+	|sed 's/ფ/f/g;s/ქ/q/g;s/ღ/g/g;s/ყ/y/g;s/შ/sh/g' \
+	|sed 's/ჩ/ch/g;s/ც/c/g;s/ძ/dz/g;s/წ/w/g;s/ჭ/tch/g' \
+	|sed 's/ხ/x/g;s/ჯ/j/g;s/ჰ/h/g' >> $2
 
 sort $2|uniq > $2".bak"; mv $2".bak" $2
 
