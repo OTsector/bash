8e1684878c3b964f7003d7bd114e6b606dcde62d
OT <imot@protonmail.com>
2020-07-02 11:23:52 +0400

    Update kaen.sh

--- kaen.sh
+++ kaen.sh
@@ -18,8 +18,7 @@ cat $1|sed 's/ა/a/g;s/ბ/b/g;s/გ/g/g;s/დ/d/g;s/ე/e/g' \
 	|sed 's/ლ/l/g;s/მ/m/g;s/ნ/n/g;s/ო/o/g;s/პ/p/g' \
 	|sed 's/ჟ/j/g;s/რ/r/g;s/ს/s/g;s/ტ/t/g;s/უ/u/g' \
 	|sed 's/ფ/f/g;s/ქ/q/g;s/ღ/g/g;s/ყ/y/g;s/შ/sh/g' \
-	|sed 's/ჩ/ch/g;s/ც/c/g;s/ძ/dz/g;s/წ/w/g;s/ჭ/tch/g' \
+	|sed 's/ჩ/ch/g;s/ც/c/g;s/ძ/dz/g;s/წ/w/g;s/ჭ/ch/g' \
 	|sed 's/ხ/x/g;s/ჯ/j/g;s/ჰ/h/g' >> $2
 
 sort $2|uniq > $2".bak"; mv $2".bak" $2
-
