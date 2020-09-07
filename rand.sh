119cf6732170d9edf962f14fd79dac50bde6bbf4
OT <imot@protonmail.com>
2020-09-07 15:24:08 +0400

    Create rand.sh

---
+++ rand.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function rand {
+	[ $# -lt 2 ] && { echo "use: [string] [length]";return 1; }
+	local s="$2"
+	local a="$1"
+	local b=
+	for((i=0;i<$s;i++));do
+		b+=${a:$[$RANDOM % ${#a}]:1}
+	done
+	echo $b
+}
+
+rand "$1" "$2"
