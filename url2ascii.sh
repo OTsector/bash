b1eba824f23772ae61bddff96ce871fcb3789a5b
OT <imot@protonmail.com>
2024-01-04 19:18:49 +0400

    Add files via upload

---
+++ url2ascii.sh
@@ -0,0 +1,19 @@
+
+function url2ascii {
+	local text="$*"
+	local out=
+	for((i=0; i<${#text}; i++)); do
+		c=${text:$i:1}
+		if [[ "$c" == "%" ]]; then
+			out+="\x${text:$i+1:1}${text:$i+2:1}"
+			let i+=2
+		elif [[ "$c" == "+" ]]; then
+			out+=" "
+		else
+			out+="$c"
+		fi 
+	done
+	printf "${out}"
+}
+
+url2ascii "$@"
