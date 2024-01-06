c31c57684aeacdd68188209685b6cdf32c686496
OT <imot@protonmail.com>
2024-01-06 15:46:31 +0400

    Add files via upload

--- ascii2url.sh
+++ ascii2url.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 function ascii2url {
-	local text="$*"
-	local out=
-	for((i=0; i<${#text}; i++)); do
-		c=${text:$i:1}
-		case $c in
-			[a-zA-Z0-9.~_-]) out+="$c" ;;
-			*) out+=`printf '%%%02X' "'$c"` ;;
-		esac
+	local inputString="$1" buf= hex= int= out=
+	for ((i=0; i<${#inputString}; i++)); do
+		char="${inputString:$i:1}"
+		hex=$(printf "%04x" "'$char")
+		int="$((0x$hex))"
+		if ((int >= 0 && int <= 0x7F)); then
+			out+=$char
+		elif ((int >= 0x80 && int <= 0x7FF)); then
+			buf=$(printf '%02x%02x' $((0xC0 | ((int >> 6) & 0x1F))) $((0x80 | (int & 0x3F))))
+			for((j=0; j<${#buf}; j+=2)); do
+				out+="\x${buf:$j:2}"
+			done
+		elif ((int >= 0x800 && int <= 0xFFFF)); then
+			buf=$(printf '%02x%02x%02x' $((0xE0 | ((int >> 12) & 0x0F))) $((0x80 | ((int >> 6) & 0x3F))) $((0x80 | (int & 0x3F))))
+			for((j=0; j<${#buf}; j+=2)); do
+				out+="%${buf:$j:2}"
+				echo "%${buf:$j:2}"
+			done
+		fi
 	done
 	echo $out
 }
