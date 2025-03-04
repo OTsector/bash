df2e9ef27ea15f86f3e695637f0adbe7d3242eda
OT <imot@protonmail.com>
2025-03-04 21:31:54 +0400

    Update ascii2url.sh

--- ascii2url.sh
+++ ascii2url.sh
@@ -4,20 +4,27 @@ function ascii2url {
 	local inputString="$1" buf= hex= int= out=
 	for ((i=0; i<${#inputString}; i++)); do
 		char="${inputString:$i:1}"
-		hex=$(printf "%04x" "'$char")
+		hex=$(printf "%04X" "'$char")
 		int="$((0x$hex))"
-		if ((int >= 0 && int <= 0x7F)); then
+		if ((int >= 0x30 && int <= 0x39)) \
+			|| ((int >= 0x41 && int <= 0x5A)) \
+			|| ((int >= 0x61 && int <= 0x7A)); then
 			out+=$char
 		elif ((int >= 0x80 && int <= 0x7FF)); then
-			buf=$(printf '%02x%02x' $((0xC0 | ((int >> 6) & 0x1F))) $((0x80 | (int & 0x3F))))
-			for((j=0; j<${#buf}; j+=2)); do
-				out+="\x${buf:$j:2}"
-			done
-		elif ((int >= 0x800 && int <= 0xFFFF)); then
-			buf=$(printf '%02x%02x%02x' $((0xE0 | ((int >> 12) & 0x0F))) $((0x80 | ((int >> 6) & 0x3F))) $((0x80 | (int & 0x3F))))
+			buf=$(printf '%02X%02X' $((0xC0 | ((int >> 6) & 0x1F))) $((0x80 | (int & 0x3F))))
 			for((j=0; j<${#buf}; j+=2)); do
 				out+="%${buf:$j:2}"
-				echo "%${buf:$j:2}"
+			done
+		elif ((int >= 0x00 && int <= 0x7F)); then
+			buf=$(printf '%02X' $((int)))
+			for((j=0; j<${#buf}; j+=2)); do
+				out+="%${buf:$j:2}"
+			echo buf $buf char: $char
+			done
+		elif ((int >= 0x800 && int <= 0xFFFF)); then
+			buf=$(printf '%02X%02X%02X' $((0xE0 | ((int >> 12) & 0x0F))) $((0x80 | ((int >> 6) & 0x3F))) $((0x80 | (int & 0x3F))))
+			for((j=0; j<${#buf}; j+=2)); do
+				out+="%${buf:$j:2}"
 			done
 		fi
 	done
